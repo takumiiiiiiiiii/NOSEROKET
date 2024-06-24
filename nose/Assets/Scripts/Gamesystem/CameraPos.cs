cb027b71ef1ef15e3ee2cde6b5179ab3af604ce7
takumiiiiiiiiii <136224249+takumiiiiiiiiii@users.noreply.github.com>
2024-06-24 16:06:18 +0900

    画面を固定しながらのパターン

---
+++ nose/Assets/Scripts/Gamesystem/CameraPos.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour
+{
+    GameObject Nose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Nose = GameObject.Find("nose_player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(Nose.transform.position.x, Nose.transform.position.y,-10);
+    }
+}
