a1477777e86a58b0d8f11a6a980c2998685d5469
takumiiiiiiiiii <136224249+takumiiiiiiiiii@users.noreply.github.com>
2024-06-24 15:07:37 +0900

    惑星吹っ飛ばす

--- nose/Assets/Scripts/Character/Nosemove.cs
+++ nose/Assets/Scripts/Character/Nosemove.cs
@@ -7,9 +7,11 @@ public class Nosemove : MonoBehaviour
     // Start is called before the first frame update
     public Rigidbody2D RB;
     public float speed;//鼻の移動スピードを入力する
+    public float dash_speed=2;//ダッシュ時のスピード
     public Vector2 player_vector;//プレイヤーの現在地を記録
     private Vector2 forword;
-    private bool Nose_Dush=false;
+    [HideInInspector] public static bool DoNotMove=false;
+    [HideInInspector] public  bool Nose_Dush=false;
     void Start()
     {
         forword = transform.forward;
@@ -32,28 +34,30 @@ public class Nosemove : MonoBehaviour
         worldAngle.x = 10.0f;
         worldAngle.y = 0;
         worldAngle.z = 0;
-        
-        if (Input.GetKey(KeyCode.LeftArrow)&& Input.GetKey(KeyCode.Space) == false)
-        {
-            myTransform.Rotate(0, 0, 3.0f, Space.World);
-        }
-        if (Input.GetKey(KeyCode.RightArrow)&&Input.GetKey(KeyCode.Space) == false)
-        {
-            myTransform.Rotate(0, 0, -3.0f, Space.World);
-        }
-        if (Input.GetKey(KeyCode.Space)==false)
-        {
-            if (Nose_Dush == true)//
+        Debug.Log("DoNotMove:" + DoNotMove);
+        if (DoNotMove == false){
+            if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Space) == false)
             {
-                transform.position += transform.rotation * new Vector2(0, speed * 2);//ダッシュ
+                myTransform.Rotate(0, 0, 3.0f, Space.World);
             }
-            else
+            if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.Space) == false)
             {
-                transform.position += transform.rotation * new Vector2(0, speed);//通常の移動
+                myTransform.Rotate(0, 0, -3.0f, Space.World);
             }
-        }else if(Nose_Dush == true)
-        {
-            transform.position += transform.rotation * new Vector2(0, speed * 2);//ダッシュ
+            if (Input.GetKey(KeyCode.Space) == false)
+            {
+                if (Nose_Dush == true)//
+                {
+                    transform.position += transform.rotation * new Vector2(0, speed * dash_speed);//ダッシュ
+                }
+                else
+                {
+                    transform.position += transform.rotation * new Vector2(0, speed);//通常の移動
+                }
+            } else if (Nose_Dush == true)
+            {
+                transform.position += transform.rotation * new Vector2(0, speed * dash_speed);//ダッシュ
+            }
         }
     }
     private IEnumerator Dash()
---
+++ nose/Assets/Scripts/Character/pop_upPolllen.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class pop_upPolllen : MonoBehaviour
+{
+    [SerializeField] private CircleCollider2D CC;
+    [SerializeField] private Rigidbody2D RB;
+    public float speed;
+    public float pop_min=100f,pop_max=200f;
+    private Vector2 vec;//鼻のベクトルを入れる
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        CC = GetComponent<CircleCollider2D>();
+        RB.AddForce(new Vector2(Random.Range(pop_min, pop_max), Random.Range(pop_min,pop_max)));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Nosemove nose;//呼ぶスクリプトにあだ名をつける
+        GameObject obj = GameObject.Find("nose_player");//Circleというゲームオブジェクトを探す
+        nose = obj.GetComponent<Nosemove>();//スクリプトを取得
+        Vector2 Pvec = new Vector2(nose.player_vector.x, nose.player_vector.y);//プレイヤーの座標を保存
+        vec = Pvec - new Vector2(this.transform.position.x, this.transform.position.y);//プレイヤーの位置から敵の位置を引く
+        vec = vec.normalized*speed;//正規化
+        StartCoroutine(Homing());
+        RB.AddForce(vec*speed);
+        //RB.velocity = vec * speed;
+    }
+    private IEnumerator Homing()
+    {
+        
+        yield return new WaitForSeconds(0.5f);
+        RB.velocity = vec * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
