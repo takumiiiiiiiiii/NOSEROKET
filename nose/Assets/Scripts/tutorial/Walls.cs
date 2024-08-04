b4ff77ffeb9431c6bb580bd7d904bd027cbd1675
takumiiiiiiiiii <136224249+takumiiiiiiiiii@users.noreply.github.com>
2024-08-04 23:44:27 +0900

    チュートリアルの作成

---
+++ nose/Assets/Scripts/tutorial/Walls.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Walls : MonoBehaviour
+{
+    Rigidbody2D RB;
+    BoxCollider2D BC;
+    private bool dilaybo=false;//鼻を弾き飛ばした後連続で弾き飛ばさないようにする。
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        BC = GetComponent<BoxCollider2D>();
+        RB.gravityScale = 0;
+        BC.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform parentTransform = transform.parent;
+        if (parentTransform == null)
+        {
+            RB.gravityScale = 1.3f;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Nosemove nose;//呼ぶスクリプトにあだ名をつける
+        GameObject obj = GameObject.Find("nose_player");//Circleというゲームオブジェクトを探す
+        nose = obj.GetComponent<Nosemove>();//スクリプトを取得
+        if (collision.gameObject.tag == "Player"&&dilaybo==false)
+        {
+            
+            StartCoroutine(Dilay());
+            Nosemove.myTransform.Rotate(0, 0, 180f, Space.World);
+            
+        }
+    }
+    private IEnumerator Dilay()//鼻を一時的にフリーズさせる
+    {
+        dilaybo = true;
+        yield return new WaitForSeconds(1);
+        dilaybo = false;
+    }
+}
---
+++ nose/Assets/Scripts/tutorial/tutorial_PlanetBreak.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class tutorial_PlanetBreak : MonoBehaviour
+{
+    public float speed = 40;//吹っ飛んで行くスピード
+    public int OUT_POLLEN = 20;//落とす花粉の数
+    public string scene_name = "kakehi_open";
+    [SerializeField] private GameObject pollen;//花粉
+
+    private bool Hit = false;//ダッシュ中の鼻にあたったかを判定
+    private Vector2 vec;//鼻のベクトルを入れる
+    private bool vector_get = true;//当たった瞬間のベクトルを取得する
+    Rigidbody2D RB;
+    CircleCollider2D CC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        CC = GetComponent<CircleCollider2D>();
+        RB.gravityScale = 0;
+        RB.mass = 0.1f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Nosemove nose;//呼ぶスクリプトにあだ名をつける
+        GameObject obj = GameObject.Find("nose_player");//Circleというゲームオブジェクトを探す
+        nose = obj.GetComponent<Nosemove>();//スクリプトを取得
+        if (Hit == true)
+        {
+            if (vector_get == true)
+            {
+                this.gameObject.transform.DetachChildren();
+                for (int i = 0; i < OUT_POLLEN; i++)
+                {
+                    Instantiate(pollen, this.transform.position, Quaternion.identity);//花粉を置く
+                }
+                Debug.Log("飛ぶ");
+                Vector2 Pvec = new Vector2(nose.player_vector.x, nose.player_vector.y);//プレイヤーの座標を保存
+                vec = Pvec - new Vector2(this.transform.position.x, this.transform.position.y);//プレイヤーの位置から敵の位置を引く
+                vec = vec.normalized;//正規化
+                vector_get = false;
+
+            }
+            StartCoroutine(End_tutorial());
+            RB.velocity = -vec * speed;
+        }
+    }
+    private IEnumerator Stopc_MoveNose()//鼻を一時的にフリーズさせる
+    {
+        Nosemove.DoNotMove = true;
+        yield return new WaitForSeconds(1);
+        Nosemove.DoNotMove = false;
+    }
+    private IEnumerator End_tutorial()//シーンを移動
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(scene_name);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Nosemove nose;//呼ぶスクリプトにあだ名をつける
+        GameObject obj = GameObject.Find("nose_player");//Circleというゲームオブジェクトを探す
+        nose = obj.GetComponent<Nosemove>();//スクリプトを取得
+        if (collision.gameObject.tag == "Player" && Nosemove.Nose_Dush == true)
+        {
+            StartCoroutine(Stopc_MoveNose());
+            Hit = true;
+        }
+    }
+}
