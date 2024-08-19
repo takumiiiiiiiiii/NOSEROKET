87c5bfb7ab13082e98b414e5a0aab2650afa2249
takumiiiiiiiiii <136224249+takumiiiiiiiiii@users.noreply.github.com>
2024-08-19 12:27:27 +0900

    センサーに対応

--- nose/Assets/Gamesystem/Start_botton.cs
+++ nose/Assets/Gamesystem/Start_botton.cs
@@ -10,6 +10,8 @@ public class Start_botton : MonoBehaviour
     SpriteRenderer MainSpriteRenderer;
     public Sprite changeSprite;
 
+    private float x_before = 10;
+    private float z_before = 10;
     private bool anime_start = false;
     private float add_spd = -11.0f;  
     // Start is called before the first frame update
@@ -21,15 +23,33 @@ public class Start_botton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Nosemove.growlevel);
+        Serial serial;//呼ぶスクリプトにあだ名をつける
+        GameObject objc = GameObject.Find("sencer");//Circleというゲームオブジェクトを探す
+        serial = objc.GetComponent<Serial>();//スクリプトを取得
+        float x, z;
+        if (float.TryParse(serial.x, out x) && float.TryParse(serial.z, out z))
+        {
+
+            if (x_before < Nosemove.growlevel && z_before < Nosemove.growlevel)
+            {
+                if (x >= Nosemove.growlevel || z >= Nosemove.growlevel)
+                {
+                    anime_start = true;
+                    MainSpriteRenderer.sprite = changeSprite;
+                }
+            }
+            x_before = x;
+            z_before = z;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space)) //スペースキー押した場合
         {
             //SceneManager.LoadScene("SampleScene");//some_senseiシーンをロードする
             anime_start = true;
             MainSpriteRenderer.sprite = changeSprite;
             Debug.Log("space");
-
         }
-        
     }
 
     void FixedUpdate()
