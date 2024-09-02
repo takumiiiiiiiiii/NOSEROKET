530987e5b980e0523369a9fc2fefc33f49c11cdd
takumiiiiiiiiii <136224249+takumiiiiiiiiii@users.noreply.github.com>
2024-09-02 15:34:32 +0900

    動画を入れる

--- nose/Assets/Scripts/Character/Pollen/pop_upPolllen.cs
+++ nose/Assets/Scripts/Character/Pollen/pop_upPolllen.cs
@@ -22,7 +22,15 @@ public class pop_upPollen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        targetCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        // カメラを探す
+        GameObject cameraObject = GameObject.Find("MainCamera");
+        // カメラが見つかった場合のみコンポーネントを取得する
+        if (cameraObject != null)
+        {
+            Camera targetCamera = cameraObject.GetComponent<Camera>();
+            // targetCameraでさらに処理を続ける
+        }
+        //targetCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
         //カメラのいちを取得
         if (targetCamera == null)
         {
